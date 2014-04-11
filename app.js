276ad701a6bf435a0de2498f5768ec92ce84127b
Erik <ejderooij@gmail.com>
2014-04-11 19:58:41 +0200

    Small change

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var http = require('http');
 
 http.createServer(function(req,resp) {
     resp.writeHead(200, {"Content-Type": "text/plain"});
-    resp.write("Hello World2");
+    resp.write("Hello World3");
     resp.end();
 
     console.log("sample output to console");
