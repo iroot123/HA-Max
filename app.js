ff16307db62cbf3a43d601bdb6c8aafd1c98272b
Erik <ejderooij@gmail.com>
2014-04-27 22:46:16 +0200

    Update check

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var http = require('http');
 
 http.createServer(function(req,resp) {
     resp.writeHead(200, {"Content-Type": "text/plain"});
-    resp.write("Hello World4");
+    resp.write("Hello World5");
     resp.end();
 
     console.log("sample output to console");
